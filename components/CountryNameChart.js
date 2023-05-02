6d2b752696bbf0ba4f1e3d540d5c43f6f6e3a4fd
@EnaidaH <enaidahuskic@yahoo.com>
2023-05-02 13:03:22 +0200

    fix bar chart

--- components/CountryNameChart.js
+++ components/CountryNameChart.js
@@ -67,10 +67,26 @@ const CountryNameChart = () => {
     labels: countryNames.map((x) => x.countryName),
     datasets: [
       {
-        label: countryNames.map((x) => x.countryName),
-        data: [countryNames.map((x) => x.occurrence)],
-        borderColor: ['rgba(255, 99, 132, 1)'],
-        backgroundColor: ['rgba(255, 99, 132, 0.2)'],
+        data: countryNames.map((x) => x.occurrence),
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)',
+        ],
+        borderWidth: 1,      
+        maxBarThickness: 8,
+        minBarLength: 2,
       },
     ],
   }
