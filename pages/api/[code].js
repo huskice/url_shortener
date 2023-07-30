5466be682a707ecd416f225c1ff73a599ccacbb1
@EnaidaH <enaidahuskic@yahoo.com>
2023-07-30 20:53:08 +0200

    change status code for redirect

--- pages/api/[code].js
+++ pages/api/[code].js
@@ -1,11 +1,11 @@
 import prisma from '@/lib/prisma'
 
-export default async function getUrlHandler (req, res) {
+export default async function getUrlHandler(req, res) {
   if (req.method == 'GET') {
     const { code } = req.query
     try {
       const response = await prisma.url.findUnique({ where: { code: code } })
-      res.status(200).redirect(response.originalUrl)
+      res.status(302).redirect(response.originalUrl)
     } catch (error) {
       res.status(500).json({ message: 'Something went wrong!' })
     }
